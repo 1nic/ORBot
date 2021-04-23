d0301979c162fbb957f77c2c9a9a1884020868b8
nic <nicolasfm2007@gmail.com>
2021-04-23 10:15:42 -0300

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -3,8 +3,8 @@ module.exports = {
 	description: 'announcement',
 	execute(message, client) {
         const args = message.content.slice("or ".length).trim().split(/ +/);
-		const context = args.join(" ").toLowerCase().split(/ ann/);
+		const context = args.join(" ").toLowerCase().slice(2, 2)
 		const channel = client.channels.cache.find(ch => ch.id === '828358344413937675');
-		channel.send(real);
+		channel.send(context);
 	},
 };
