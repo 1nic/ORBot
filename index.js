772cb2ef4469bbb95ec22f2a2316f6163cfcc26f
nic <nicolasfm2007@gmail.com>
2021-04-23 10:03:09 -0300

    bb

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ client.once('ready', () => {
 });
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const cmd = require(`./commands/${file}`);
 
-	client.commands.set(command.name, command);
+	client.commands.set(cmd.name, cmd);
 }
 
 client.on('message', message => {
@@ -20,7 +20,6 @@ client.on('message', message => {
 	if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(`**${message.author.tag}** you dont have permission to do that`);
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const cmdName = args.shift().toLowerCase();
-	if (!client.commands.has(command)) return message.channel.reply(`${message.author.tag} what command is that? you dumb`);
 	const command = client.commands.get(commandName);
 	if (cmdName === 'ann') {
 		command.execute(message);
