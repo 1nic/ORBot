34791ca8d0465038f0d2306dd2b23ebaedde6822
nic <nicolasfm2007@gmail.com>
2021-04-23 11:52:39 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'kick',
 	description: 'kick',
 	execute(message, vic) {
-		message.mentions.first().kick();
-        message.channel.send(`${message.mentions.first().name} were kicked.`);
+		message.members.mentions.first().kick()
+        message.channel.send(`${vic.name} were kicked.`);
 	},
 };
