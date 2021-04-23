07aaf2dd4ffca34728ce98baeb84e1758ade754f
nic <nicolasfm2007@gmail.com>
2021-04-23 18:08:27 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,7 +6,7 @@ module.exports = {
 		if (!message.mentions.members.first()) return message.reply('you want to kick the null?');
 		const member = message.mentions.members.first()
 		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} is the same/higher rank than you`);
-		const reason = message.slice(1).join(' ') || 'nothing/none reason';
+		const reason = message.content.slice(1).join(' ') || 'nothing/none reason';
 		member.kick({ reason });
         message.channel.send(`${member} were kicked for ${reason}.`);
 	},
