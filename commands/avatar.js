d684aabb6366f49381bed31833eaa1049b304268
nic <nicolasfm2007@gmail.com>
2021-04-23 10:45:49 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message){
-		message.send(`${message.author.tag}'s avatar:`, {files: message.author.displayAvatarURL({format : 'png', dynamic : true})});
+		message.channel.send(`${message.author.tag}'s avatar:`, {files: message.author.displayAvatarURL({format : 'png', dynamic : true})});
 	},
 };
