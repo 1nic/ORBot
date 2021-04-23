b6370d4e616ce4e90a84fc56b4340e7e8cba13ae
nic <nicolasfm2007@gmail.com>
2021-04-23 16:18:58 -0300

    unban update

--- commands/unban.js
+++ commands/unban.js
@@ -2,14 +2,12 @@ module.exports = {
 	name: 'unban',
 	description: 'unban',
 	run: async(message, args) => {
-		execute(() => {
-			if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply('go get ban perms and then do the command again, kiddo.');
-			const id = args[1]
-			if (!id) return message.reply('you gotta use the user id, i cant unban people by usernames or else.')
-			const bannedppl = await message.guild.fetchBans();
-			if (!bannedppl.find((user) => user.user.id === id)) return message.reply(`user is not a valid banned user`);
-			message.guild.members.unban(id);
-       		 message.channel.send(`the user were unbanned`);
-		})
+		if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply('go get ban perms and then do the command again, kiddo.');
+		const id = args[1]
+		if (!id) return message.reply('you gotta use the user id, i cant unban people by usernames or else.')
+		const bannedppl = await message.guild.fetchBans();
+		if (!bannedppl.find((user) => user.user.id === id)) return message.reply(`user is not a valid banned user`);
+		message.guild.members.unban(id);
+    	message.channel.send(`the user were unbanned`);
 	}
 };
