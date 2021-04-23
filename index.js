5ea29b53a6401206228a9f964fe31cda7c6278f9
nic <nicolasfm2007@gmail.com>
2021-04-23 10:04:45 -0300

    bbb

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('message', message => {
 	if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(`**${message.author.tag}** you dont have permission to do that`);
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const cmdName = args.shift().toLowerCase();
-	const command = client.commands.get(commandName);
+	const command = client.commands.get(cmdName);
 	if (cmdName === 'ann') {
 		command.execute(message);
 	} else if (cmdName === 'kick') {
