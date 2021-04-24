87eac8f75bab7e7bbbbf0876545beaafb5b8a685
nic <nicolasfm2007@gmail.com>
2021-04-24 14:01:49 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,7 +5,7 @@ module.exports = {
 		if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply('go get kick perms lmao');
 		if (!message.mentions.members.first()) return message.reply('you want to kick the null?');
 		const member = message.mentions.members.first()
-		const bot = message.guild.cache.get(client.user.id);
+		const bot = message.guild.cache.get('834495830869409843');
 		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} what are you trying to do, man?`);
 		if (bot.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} i dont have permission to ban the user.`);
 		const args = message.content.slice("or ".length).trim().split(/ +/);
