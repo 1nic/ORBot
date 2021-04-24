20e4286582556d55b5f0733ee9fbc12bcbd5e946
nic <nicolasfm2007@gmail.com>
2021-04-24 13:45:26 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,13 +1,15 @@
 module.exports = {
 	name: 'kick',
 	description: 'kick',
-	execute(message) {
-		if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply('go get kick perms and then do the command again, kiddo.');
+	run: async(message) =>{
+		if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply('go get kick perms lmao');
 		if (!message.mentions.members.first()) return message.reply('you want to kick the null?');
 		const member = message.mentions.members.first()
-		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} is the same/higher rank than you`);
-		const reason = message.content.slice(1).join(' ') || 'nothing/none reason';
+		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} what are you trying to do, man?`);
+		const args = message.content.slice("or ".length).trim().split(/ +/);
+		const pre = args.join(" ").toLowerCase();
+		const context = pre.split('ann');
+		const reason = context.slice(2).join(' ') || 'none';
 		member.kick({ reason });
-        message.channel.send(`${member} were kicked for ${reason}.`);
-	},
-};
+        message.channel.send(`${member} were banned for ${reason}.`);
+}}
