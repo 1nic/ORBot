ceec9433544cf9389782a1ad9145ba6c45290fac
nic <nicolasfm2007@gmail.com>
2021-04-24 15:22:37 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -7,7 +7,6 @@ module.exports = {
 		if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply('go get kick perms lmao');
 		if (!message.mentions.members.first()) return message.reply('you want to kick the null?');
 		const member = message.mentions.members.first()
-		const bot = message.guild.cache.get('834495830869409843');
 		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} what are you trying to do, man?`);
 		const args = message.content.slice("or ".length).trim().split(/ +/);
 		const pre = args.join(" ").toLowerCase();
