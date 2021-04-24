568c658f51a5e3af9e076b5b9a329acdca84e1ac
nic <nicolasfm2007@gmail.com>
2021-04-24 15:17:07 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,3 +1,5 @@
+const {Client, Message, MessageEmbed} = require('discord.js')
+
 module.exports = {
 	name: 'kick',
 	description: 'kick',
@@ -13,5 +15,15 @@ module.exports = {
 		const context = pre.split('ann');
 		const reason = context.slice(2).join(' ') || 'none';
 		member.kick({ reason });
+		const channel = client.channels.cache.find(ch => ch.id === '828358344413937675');
         message.channel.send(`${member} were kicked for ${reason}.`);
+		const embedMessage = new MessageEmbed()
+		.setTitle(`${message.author.id} kicked ${member.author.id}`)
+		.setColor('FF0000')
+		.setThumbnail(message.author.displayAvatarURL({format : 'png', dynamic : true}))
+		.addField('Reason: ', [
+			reason
+		])
+		channel.send(embedMessage)
+
 }}
