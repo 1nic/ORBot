38b87e4becb4a7d21e9cbad559c2e645bb22be7b
nic <nicolasfm2007@gmail.com>
2021-04-25 08:04:25 -0300

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -5,6 +5,6 @@ module.exports = {
     name: 'send',
     run: async(client, message, args) => {
         const image = new MessageAttachment('https://cdn.discordapp.com/attachments/835130113599209473/835824382756388864/gay_-_Copy.png');
-        message.reply(text, image)
+        message.reply('no', image)
     }
 }
