1ae15d1951a99b1bdc6c2661b25a0b1722666240
nic <nicolasfm2007@gmail.com>
2021-04-25 17:19:35 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,7 @@ module.exports = {
         if (!message.member.hasPermission('KICK_MEMBERS')) return message.reply('noob, you dont have delete message perms.');
         if (!args[0]) return message.reply('pls send da amount');
         if (isNaN(args[0])) return message.reply('numbers...');
-        if (parseInt(args[0]) > 100) return message.reply('i cannot delete more than 100 messages.');
+        if (parseInt(args[0]) > 99) return message.reply('i cannot delete more than 99 messages.');
         await message.channel.bulkDelete(parseInt(args[0] + 1))
         .catch(error => console.log(error));
         message.reply(`deleted ${parseInt(args[0])} messages`)
