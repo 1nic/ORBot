d5bf015b960adcea06cf4821278388371ef8bf51
nic <nicolasfm2007@gmail.com>
2021-04-25 17:49:53 -0300

    Update real.js

--- commands/others/real.js
+++ commands/others/real.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'real',
     run: async(client, message, args) => {
-       if (!args.indexOf('real') == -1) return console.log('yeaj')
+       if (args.indexOf('real') > -1) return console.log('yeaj')
     }
 }
