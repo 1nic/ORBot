047d0130d4abdca812dc68a1f04bfb1807daf631
nic <nicolasfm2007@gmail.com>
2021-04-25 17:02:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-	const args = message.content.trim().split(/ +/);
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const other = client.othercommands.get(otherName);
 	console.log(args[0], otherName);
