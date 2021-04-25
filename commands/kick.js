6df387d37ba1d110ce7b7a29234a765294093400
nic <nicolasfm2007@gmail.com>
2021-04-25 10:21:50 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,8 +5,8 @@ module.exports = {
 	description: 'kick',
 	run: async(client, message, args) =>{
 		if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply('go get kick perms lmao');
-		const member = message.mentions.members.size
-		if (!member) return console.log("something is wrong, dude.");
+		if (!message.mentions.members.first()) return console.log('monke');
+		const member = message.mentions.members.first()
 		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} what are you trying to do, man?`);
 		const context = args[0]
 		const reason = args.slice(1, args.length) || 'none';
