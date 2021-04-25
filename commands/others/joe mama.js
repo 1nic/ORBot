e2810ad744cbbf7f931ca6da60ba29bbb1825ef6
nic <nicolasfm2007@gmail.com>
2021-04-25 16:45:07 -0300

    ;;

---
+++ commands/others/joe mama.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'joe mama',
+    run: async(client, message, args) => {
+        message.channel.send('bad joke');
+    }
+}
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const prefix = "r "
 const client = new Discord.Client();
 const fs = require('fs');
 client.commands = new Discord.Collection();
+client.othercommands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const otherFiles = fs.readdirSync('./commands/others').filter(file => file.endsWith('.js'));
 
 const msgarray = [
 	'nic is gay',
@@ -30,9 +32,14 @@ for (const file of commandFiles) {
 	client.commands.set(cmd.name, cmd);
 }
 
+for (const file of otherFiles) {
+	const cmd = require(`./commands/others/${file}`);
+
+	client.othercommands.set(cmd.name, cmd);
+}
+
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(`**${message.author.tag}** you dont have permission to do that`);
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const cmdName = args.shift().toLowerCase();
 	const command = client.commands.get(cmdName);
@@ -41,9 +48,12 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-	if (message.content == "epic") {
-		message.channel.send('epnic, use the correct grammar')
-	}
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const otherName = args.shift().toLowerCase();
+	const other = client.othercommands.get(otherName);
+	if (!other) return;
+	other.run(client, message, args);
 })
 
 client.on('message', function(message){
