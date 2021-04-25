27a92f6e6a62daf297e7eb4f7fd12b5aef486a75
nic <nicolasfm2007@gmail.com>
2021-04-25 16:57:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.on('message', message => {
 
 client.on('message', message => {
 	const args = message.content.trim().split(/ +/);
-	const otherName = args[0].toLowerCase();
+	const otherName = args.shift().toLowerCase();
 	const other = client.othercommands.get(otherName);
 	console.log(args[0], otherName);
 	if (!other) return;
