5ea7a0eb69144806c50c1a9fe4923daf44b83e54
nic <nicolasfm2007@gmail.com>
2021-04-25 08:21:51 -0300

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'ann',
 	run: async(client, message, args) => {
 		const context = args[0];
+		if (!context) return message.reply('do it again but this time write what you want to announce')
 		const channel = client.channels.cache.find(ch => ch.id === '828358344413937675');
 		channel.send(context);
 	}
