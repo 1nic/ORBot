aff344092da7f5069e43cc571e66865cc3580b5c
nic <nicolasfm2007@gmail.com>
2021-04-25 11:36:35 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'clear',
     description: 'clear messages',
     run: async(client, message, args) => {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('noob, you dont have delete message perms.');
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.reply('noob, you dont have delete message perms.');
         if (!args[0]) return message.reply('pls send da amount');
         if (isNaN(args[0])) return message.reply('numbers...');
         if (parseInt(args[0]) > 100) return message.reply('i cannot delete more than 100 messages.');
