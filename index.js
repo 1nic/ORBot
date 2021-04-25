3ec7db452f2c926963184b1f4b102df207c1e822
nic <nicolasfm2007@gmail.com>
2021-04-25 10:45:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const prefix = "or "
+const prefix = "r "
 const client = new Discord.Client();
 const fs = require('fs');
 client.commands = new Discord.Collection();
@@ -26,7 +26,7 @@ for (const file of commandFiles) {
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(`**${message.author.tag}** you dont have permission to do that`);
-	const args = message.content.slice("or ".length).trim().split(/ +/);
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const cmdName = args.shift().toLowerCase();
 	const command = client.commands.get(cmdName);
 	if (!command) return;
