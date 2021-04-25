b0b7be3609bd2edc262e79c65046712178b74af5
nic <nicolasfm2007@gmail.com>
2021-04-25 10:37:17 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,11 +6,10 @@ module.exports = {
 	run: async(client, message, args) =>{
 		console.log(message.content);
 		if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply('go get kick perms lmao');
-		if (!message.mentions.members.first()) return console.log('monke');
 		const member = message.mentions.members.first()
 		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} what are you trying to do, man?`);
 		const context = args[0]
-		const reason = args.slice(1, args.length) || 'none';
+		const reason = context.slice(context.length) || 'none';
 		member.kick({ reason });
 		const channel = client.channels.cache.find(ch => ch.id === '834078274770042921');
         message.channel.send(`${member} were kicked for ${reason}.`);
@@ -18,10 +17,12 @@ module.exports = {
 		.setTitle(`${message.author.tag} kicked ${member.user.tag}`)
 		.setColor('FF0000')
 		.setThumbnail(message.author.displayAvatarURL({format : 'png', dynamic : true}))
+		
 		.addField('Reason: ', [
 			reason
 		])
 		console.log(`reason: ${reason}`);
 		console.log(`arg = ${args[0]}`);
+		if (!reason) return;
 		channel.send(embedMessage)
 }}
