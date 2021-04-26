c1cf684942ed028c85596dda73469f120b55a455
nic <nicolasfm2007@gmail.com>
2021-04-26 17:37:02 -0300

    Create are you sure about that.js

---
+++ commands/others/are you sure about that.js
@@ -0,0 +1,9 @@
+const {MessageAttachment} = require('discord.js');
+
+module.exports = {
+    name: 'are you sure about that',
+    run: async(client, message, args) => {
+        const vido = new MessageAttachment('https://cdn.discordapp.com/attachments/832734989036421140/836339599713370203/john_mama.mp4')
+        message.channel.send(vido)
+    }
+}
