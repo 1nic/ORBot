e4e9d8b11421c9ba150216249ea5dee6e2a46300
nic <nicolasfm2007@gmail.com>
2021-04-26 20:32:26 -0300

    Update sus.js

--- commands/others/sus.js
+++ commands/others/sus.js
@@ -2,7 +2,11 @@ const {MessageAttachment} = require('discord.js')
 
 const vidos = [
     'https://cdn.discordapp.com/attachments/698665018056638604/836355810501853284/isnt_this_shit_dead.mp4',
-    'https://cdn.discordapp.com/attachments/728614774597877841/836382490284916747/imposter.png'
+    'https://cdn.discordapp.com/attachments/728614774597877841/836382490284916747/imposter.png',
+    'https://cdn.discordapp.com/attachments/835130113599209473/836383389946085437/funny_haha_pls_someone_kill_this_meme.jpg',
+    'https://cdn.discordapp.com/attachments/835130113599209473/836383932308127774/AMOGUS.mp4',
+    'https://cdn.discordapp.com/attachments/835130113599209473/836384070463782972/amogus.jpg',
+    'https://cdn.discordapp.com/attachments/835130113599209473/836384189603512370/kill.png'
 ]
 
 module.exports = {
