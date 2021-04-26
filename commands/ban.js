689052fd47174e85bfbde2edb50aa89b06268ac9
nic <nicolasfm2007@gmail.com>
2021-04-26 14:42:12 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,10 +3,11 @@ module.exports = {
 	description: 'ban',
 	run: async(client, message, args) => {
 		if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply('go get ban perms and then do the command again, kiddo.');
+		if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.reply('i dont have perms to do that');
 		if (!message.mentions.members.first()) return message.reply('you want to ban the null?');
 		const member = message.mentions.members.first()
 		if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply(`${member} is the same/higher rank than you`);
-		const context = args[0]
+		const context = args.slice(1).join(' ');
 		const reason = context || 'none';
 		member.ban({ reason });
         message.channel.send(`${member} were banned for ${reason}.`);
