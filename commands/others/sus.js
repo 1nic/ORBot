1cca53fafe52c9da66faf14f8303f5b900734fc6
nic <nicolasfm2007@gmail.com>
2021-04-26 20:26:27 -0300

    Update sus.js

--- commands/others/sus.js
+++ commands/others/sus.js
@@ -1,9 +1,15 @@
 const {MessageAttachment} = require('discord.js')
 
+const vidos = [
+    'https://cdn.discordapp.com/attachments/698665018056638604/836355810501853284/isnt_this_shit_dead.mp4',
+    'https://cdn.discordapp.com/attachments/728614774597877841/836382490284916747/imposter.png'
+]
+
 module.exports = {
     name: 'sus',
     run: async(client, message, args) => {
-        const vido = new MessageAttachment('https://cdn.discordapp.com/attachments/698665018056638604/836355810501853284/isnt_this_shit_dead.mp4');
+        const randomval = Math.floor(Math.random() * vidos.length)
+        const vido = new MessageAttachment(vidos[randomval]);
         message.channel.send(vido)
     }
 }
