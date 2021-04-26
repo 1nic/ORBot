fce6aa35780d05ca74d7999e5319e7cb8a0313e9
nic <nicolasfm2007@gmail.com>
2021-04-26 16:28:55 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         const embed = new MessageEmbed()
         .setTitle('Omniversal Revenge Bot')
         .setColor('RED')
-        .setAuthor('prefix: or')
+        .setAuthor('prefix: r')
         .setThumbnail('https://cdn.discordapp.com/attachments/835130113599209473/835836212714668052/Omniversal_Administrative.png')
         .addField('Announcement', [
             'ann: Announce something in <#828358344413937675>'
