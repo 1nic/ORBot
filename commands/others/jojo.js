aa0d2ffbd15a589d021043dfc3e7434a771e91f2
nic <nicolasfm2007@gmail.com>
2021-04-26 20:46:14 -0300

    Create jojo.js

---
+++ commands/others/jojo.js
@@ -0,0 +1,14 @@
+const {MessageAttachment} = require('discord.js')
+
+const vidos = [
+    'https://cdn.discordapp.com/attachments/828373408504741888/836381360108077126/video0-6-1.mp4'
+]
+
+module.exports = {
+    name: 'sus',
+    run: async(client, message, args) => {
+        const randomval = Math.floor(Math.random() * vidos.length)
+        const vido = new MessageAttachment(vidos[randomval]);
+        message.channel.send(vido)
+    }
+}
