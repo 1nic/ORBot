ae5aa100cfec0d39d097623190bf226d8de4e699
nic <nicolasfm2007@gmail.com>
2021-04-27 12:30:52 -0300

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'av',
 	description: 'avatar',
 	run: async(client, message, args) => {
+		
 		message.channel.send(`${message.author.tag}'s avatar: ${message.author.displayAvatarURL({format : 'png', dynamic : false})}`);
 	}
 };
