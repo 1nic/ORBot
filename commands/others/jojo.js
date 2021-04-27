dbec26efc46af140314ee283d047ba0ba9be0bc3
nic <nicolasfm2007@gmail.com>
2021-04-27 13:58:45 -0300

    Update jojo.js

--- commands/others/jojo.js
+++ commands/others/jojo.js
@@ -2,7 +2,8 @@ const {MessageAttachment} = require('discord.js')
 
 const vidos = [
     'https://cdn.discordapp.com/attachments/828373408504741888/836381360108077126/video0-6-1.mp4',
-    'https://cdn.discordapp.com/attachments/828684796980887564/836392699766374441/nic_is_epnic.mp4'
+    'https://cdn.discordapp.com/attachments/828684796980887564/836392699766374441/nic_is_epnic.mp4',
+    'https://cdn.discordapp.com/attachments/828684796980887564/836647303036141649/images_-_2021-04-27T215650.903.jpeg'
 ]
 
 module.exports = {
