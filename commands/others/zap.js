f1b86fdb4a6e1e5d352ccb5fcfc09e808581de68
nic <nicolasfm2007@gmail.com>
2021-04-30 13:08:50 -0300

    hg

--- commands/others/zap.js
+++ commands/others/zap.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "zap",
     run: async(client, message, args) => {
-        message.channel.send('epic dude and programmer :thumbsup:')
+        message.channel.send('epnic dude and programmer :thumbsup:')
     }
 }
