323ac4c2f5260f2e179b7ab50c7c8babd79ed667
nic <nicolasfm2007@gmail.com>
2021-05-05 12:17:57 -0300

    Update this game sucks.js

--- commands/others/this game sucks.js
+++ commands/others/this game sucks.js
@@ -1,7 +1,7 @@
 const {MessageAttachment} = require('discord.js');
 
 module.exports = {
-    name: 'are you sure about that',
+    name: 'this game sucks',
     run: async(client, message, args) => {
         const vido = new MessageAttachment('https://cdn.discordapp.com/attachments/827255229967237133/839520271261761596/video0.mp4')
         message.channel.send(vido)
