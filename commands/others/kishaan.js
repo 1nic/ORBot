fe6e960f456640ba0a6708310b0bd3d573e8996a
nic <nicolasfm2007@gmail.com>
2021-05-05 14:59:33 -0300

    Update kishaan.js

--- commands/others/kishaan.js
+++ commands/others/kishaan.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "kishaan",
     run: async(client, message, args) => {
-        message.channel.send('epnic trial admin :thumbsup:')
+        message.channel.send('epnic admin :thumbsup:')
     }
 }
