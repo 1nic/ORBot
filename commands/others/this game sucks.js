816c4d32f18658d6e88ad59e9c0dfce6ccad49af
nic <nicolasfm2007@gmail.com>
2021-05-11 20:50:32 -0300

    Update this game sucks.js

--- commands/others/this game sucks.js
+++ commands/others/this game sucks.js
@@ -3,7 +3,7 @@ const {MessageAttachment} = require('discord.js');
 module.exports = {
     name: 'this game sucks',
     run: async(client, message, args) => {
-        const vido = new MessageAttachment('https://cdn.discordapp.com/attachments/827255229967237133/839520271261761596/video0.mp4')
-        message.channel.send(vido)
+        const vido = new MessageAttachment('https://cdn.discordapp.com/attachments/827255229967237133/841824256945422368/video0.mp4')
+        message.reply(vido)
     }
 }
